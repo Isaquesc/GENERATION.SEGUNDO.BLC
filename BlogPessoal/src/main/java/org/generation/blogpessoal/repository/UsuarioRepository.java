d09757c4adc7341f7e934fa8e6527d39c559517e
isaquec <isaquedeco@hotmail.com>
2021-06-07 20:27:02 -0300

    Implementando Spring Security

---
+++ BlogPessoal/src/main/java/org/generation/blogpessoal/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package org.generation.blogpessoal.repository;
+
+import java.util.*;
+import org.generation.blogpessoal.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+    public Optional<Usuario> findByUsuario(String usuario);
+}
--- FarmaciaCia/src/main/java/br/com/generation/farmacia/controller/CategoriaController.java
+++ FarmaciaCia/src/main/java/br/com/generation/farmacia/controller/CategoriaController.java
@@ -1,10 +1,15 @@
 package br.com.generation.farmacia.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
@@ -34,6 +39,21 @@ public class CategoriaController {
     @GetMapping("/departamento/{departamento}")
     public ResponseEntity<List<Categoria>> getPorDepartamento(@PathVariable String departamento) {
         return ResponseEntity.ok(bancocategoria.findAllByDepartamentoContainingIgnoreCase(departamento));
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> post(@RequestBody Categoria categoria){
+        return ResponseEntity.status(HttpStatus.CREATED).body(bancocategoria.save(categoria));
+    }
+
+    @PutMapping
+    public ResponseEntity<Categoria> editar(@RequestBody Categoria categoria) {
+        return ResponseEntity.status(HttpStatus.OK).body(bancocategoria.save(categoria));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        bancocategoria.deleteById(id);
     }
 
 
